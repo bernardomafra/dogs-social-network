b92f85db90ea28245dc79fb8d938521ab8c4c535
Bernardo Martinez De Oliveira Mafra <bernardo.mafra@mundiale.com.br>
2020-09-24 21:17:17 -0300

    feat: created Login component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './components/Home'
 import Header from './components/Header'
+import Login from './components/Login'
 import Footer from './components/Footer'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Routes> 
           <Route path="/" element={<Home /> } />
+          <Route path="/login" element={<Login /> } />
         </Routes>
         <Footer />
       </BrowserRouter>

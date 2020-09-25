e0dfef7aec3dd9b6c5eb02736bed244ae19365f6
Bernardo Martinez De Oliveira Mafra <bernardo.mafra@mundiale.com.br>
2020-09-24 21:12:24 -0300

    feat: add first route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import Header from './components/Header'
+import Footer from './components/Footer'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 function App() {
   return (
-    <div >
-      <h1>Hello Dogs</h1>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes> 
+          <Route path="/" element={<Home /> } />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }

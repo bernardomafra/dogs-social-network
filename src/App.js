00e4688b8942af430afa20b8015496f0b759daa5
Bernardo Martinez De Oliveira Mafra <bernardo.mafra@mundiale.com.br>
2020-09-24 21:15:50 -0300

    feat: add home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Home from './components/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react' ;
 import styles from './header.module.css'
+import { Link } from 'react-router-dom'
 const Heeader = () => {
-  return <div>Heeader</div>
+  return <div className={styles.header}>Header
+    <nav>
+      <Link to="/" >Home</Link>
+      <Link to="/login" >Login / Criar</Link>
+    </nav>
+  
+
+  </div>
 }
 
 export default Heeader;

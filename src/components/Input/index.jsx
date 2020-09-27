6252e1b9c3bad49eb4653df4e8d71f1de7da8028
BernardOM74 <bernardo.mafra@sga.pucminas.br>
2020-09-26 21:06:44 -0300

    style: add new style to button component

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './input.module.css'
+const Input = ({ label, type, name }) => {
+  return <div>
+    <label htmlFor={name}>{label}</label>
+    <input id={name} name={name} type={type} className={styles.input} />
+  </div>
+}
+
+export default Input;

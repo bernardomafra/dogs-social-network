6b1aac9c093019ea9fe33757e54a75e324712051
BernardOM74 <bernardo.mafra@sga.pucminas.br>
2020-09-27 01:10:25 -0300

    fix: unused text and link using button style

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react' ;
+import React from 'react';
 import styles from './footer.module.css'
+
 const Footer = () => {
-  return <div className={styles.footer}>footer</div>
+  return <div className={styles.footer}></div>
 }
 
 export default Footer;

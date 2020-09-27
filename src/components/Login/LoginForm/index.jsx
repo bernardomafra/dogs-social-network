806a48c32847d11b883f88b815e896b22dec782a
BernardOM74 <bernardo.mafra@sga.pucminas.br>
2020-09-26 22:13:07 -0300

    feat: validate when submitting

--- src/components/Login/LoginForm/index.jsx
+++ src/components/Login/LoginForm/index.jsx
@@ -5,23 +5,26 @@ import Input from "../../Input";
 import useForm from '../../../hooks/useForm'
 
 const LoginForm = () => {
-  const username = useForm('email');
+  const username = useForm();
   const password = useForm();
 
   function handleSubmit(event) {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(),
-    }).then(response => {
-      console.log(response);
-      return response.json();
-    }).then(json => {
-      console.log(json);
-    })
+
+    if (username.validate() && password.validate()) {
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(),
+      }).then(response => {
+        console.log(response);
+        return response.json();
+      }).then(json => {
+        console.log(json);
+      })
+    }
   }
 
   return (

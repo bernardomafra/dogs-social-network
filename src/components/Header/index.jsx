7c070afd1188a48707e6e71dd38924db48115afc
BernardOM74 <bernardo.mafra@sga.pucminas.br>
2020-09-27 00:08:08 -0300

    feat: show logged user name at header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,16 +5,24 @@ import { ReactComponent as DogsLogo } from '../../assets/dogs.svg';
 import { UserContext } from '../../contexts/UserContext';
 
 const Heeader = () => {
-  const { username } = React.useContext(UserContext);
+  const { data } = React.useContext(UserContext);
 
   return <header className={styles.header}>
     <nav className={`${styles.nav} container`}>
       <Link className={styles.logo} to="/" aria-label="Dogs - Home">
         <DogsLogo />
       </Link>
-      <Link className={styles.login} to="/login">
-        <span>{username || 'Login / Criar'}</span>
-      </Link>
+      {data ?
+        (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            <span>Login / Criar</span>
+          </Link>
+        )
+      }
     </nav>
 
 

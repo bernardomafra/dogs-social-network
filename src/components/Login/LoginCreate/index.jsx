3fea4431a18cddc721db79f0e061c4635ba75088
BernardOM74 <bernardo.mafra@sga.pucminas.br>
2020-09-27 01:20:20 -0300

    feat: create new type for password on useForm hook

--- src/components/Login/LoginCreate/index.jsx
+++ src/components/Login/LoginCreate/index.jsx
@@ -1,6 +1,28 @@
-import React from 'react' ;
+import React from 'react';
+import Button from '../../Button';
+import Input from '../../Input';
+import useForm from '../../../hooks/useForm'
+
 const LoginCreate = () => {
-  return <div>LoginCreate</div>
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm('password');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  )
 }
 
 export default LoginCreate;
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -5,6 +5,10 @@ const validationTypes = {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preeencha um email válido',
   },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message: 'A senha precisa ter 1 caracter maiúsculo, 1 minúscilo e 1 dígito. Com no mínimo 8 caracteres.'
+  }
 }
 
 const useForm = (validationType) => {

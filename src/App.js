f54dc372a0b12a12799533b0ca98600cc4c6bd95
BernardOM74 <bernardo.mafra@sga.pucminas.br>
2020-09-27 20:34:15 -0300

    feat: created user component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Footer from './components/Footer';
 import Home from './components/Home';
 import Header from './components/Header';
 import Login from './components/Login';
+import User from './components/User';
+
 import { UserStorage } from './contexts/UserContext'
 
 function App() {
@@ -17,7 +19,8 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
---
+++ src/components/User/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './user.module.css'
+
+const User = () => {
+  return <div className={styles.user}>Usuário</div>
+}
+
+export default User;

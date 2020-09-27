9467fcf8d62ea18dd3a6ccaa47fc5d4e5761c594
BernardOM74 <bernardo.mafra@sga.pucminas.br>
2020-09-27 00:03:25 -0300

    feat: create UserContext to use as storage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './App.css';
 
@@ -7,16 +7,20 @@ import Footer from './components/Footer';
 import Home from './components/Home';
 import Header from './components/Header';
 import Login from './components/Login';
+import { UserStorage } from './contexts/UserContext'
+
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes> 
-          <Route path="/" element={<Home /> } />
-          <Route path="/login/*" element={<Login /> } />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,22 @@
-import React from 'react' ;
-import styles from './header.module.css'
-import { Link } from 'react-router-dom'
-import {ReactComponent as DogsLogo} from '../../assets/dogs.svg' 
+import React from 'react';
+import styles from './header.module.css';
+import { Link } from 'react-router-dom';
+import { ReactComponent as DogsLogo } from '../../assets/dogs.svg';
+import { UserContext } from '../../contexts/UserContext';
+
 const Heeader = () => {
+  const { username } = React.useContext(UserContext);
+
   return <header className={styles.header}>
     <nav className={`${styles.nav} container`}>
-      <Link className={styles.logo} to="/" aria-label="Dogs - Home"> 
-        <DogsLogo/>
+      <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+        <DogsLogo />
       </Link>
       <Link className={styles.login} to="/login">
-         <span>Login / Criar</span> 
+        <span>{username || 'Login / Criar'}</span>
       </Link>
     </nav>
-  
+
 
   </header>
 }

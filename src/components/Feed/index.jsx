9fdce47f5ac9b0ac1478688835a42d838494ae6a
Bernardo Martinez De Oliveira Mafra <bernardo.mafra@mundiale.com.br>
2020-10-01 07:40:11 -0300

    feat: initialized all feed structure and its components

--- src/components/Feed/index.jsx
+++ src/components/Feed/index.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import styles from './feed.module.css'
+import React from "react";
+import FeedModal from "./FeedModal";
+import FeedPhotos from "./FeedPhotos";
 
 const Feed = () => {
-  return <div className={styles.feed}>Feed</div>
-}
+  return (
+    <div>
+      <FeedModal />
+      <FeedPhotos />
+    </div>
+  );
+};
 
 export default Feed;

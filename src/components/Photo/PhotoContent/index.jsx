938a72f6f9408f167627da509a96328a367c222d
Bernardo Martinez De Oliveira Mafra <bernardo.mafra@mundiale.com.br>
2020-10-02 07:57:38 -0300

    style: photo content set to grid in modal

--- src/components/Photo/PhotoContent/index.jsx
+++ src/components/Photo/PhotoContent/index.jsx
@@ -9,21 +9,21 @@ const PhotoContent = ({ data }) => {
     <div className={styles.photo}>
       <div className={styles.img}>
         <img src={photo.src} alt={photo.title} />
-        <div className={styles.details}>
-          <p>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
-            <span className={styles.viewers}>{photo.acessos}</span>
-          </p>
-          <h1 className="title">
-            <Link to={`foto/${photo.id}`}>{photo.title}</Link>
-          </h1>
-          <ul className={styles.attributes}>
-            <li>{photo.peso} kg</li>
-            <li>{photo.idade} anos</li>
-          </ul>
-        </div>
       </div>
-      <PhotoComments id={photo.id} comments={comments} />
+      <div className={styles.details}>
+        <p>
+          <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+          <span className={styles.viewers}>{photo.acessos}</span>
+        </p>
+        <h1 className="title">
+          <Link to={`foto/${photo.id}`}>{photo.title}</Link>
+        </h1>
+        <ul className={styles.attributes}>
+          <li>{photo.peso} kg</li>
+          <li>{photo.idade} anos</li>
+        </ul>
+        <PhotoComments id={photo.id} comments={comments} />
+      </div>
     </div>
   );
 };

6412723681259c6ea7c82967f6eb7ac1cada90fe
Bernardo Martinez De Oliveira Mafra <bernardo.mafra@mundiale.com.br>
2020-10-02 07:29:17 -0300

    feat: initialized feed modal

--- src/components/Feed/FeedModal/index.jsx
+++ src/components/Feed/FeedModal/index.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styles from "./feedmodal.module.css";
+import useFetch from "../../../hooks/useFetch";
+import Error from "../../Error";
+import Loading from "../../Loading";
+import PhotoContent from "../../PhotoContent";
+import { PHOTO_GET } from "../../../services/api";
+const FeedModal = ({ photo }) => {
+  const { data, error, loading, request } = useFetch();
 
-const FeedModal = () => {
-  return <div>FeedModal</div>;
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  return (
+    <div className={styles.modal}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 };
 
 export default FeedModal;
